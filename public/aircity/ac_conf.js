9f7c6c16bb6c19a6d43d9d8cf724b2a29edc3d32
Mr_Pardon <liaoruihong@freedotech.com>
2025-08-20 15:02:11 +0800

    bugfix: 修复build问题

--- public/aircity/ac_conf.js
+++ public/aircity/ac_conf.js
@@ -4,7 +4,7 @@
  * Path: The absolute path where the SDK folder is located (for setting the path of test data in the sample code)
  */
 var HostConfig = {
-  "Player": "127.0.0.1:8090",
+  "Player": "127.0.0.1:8010",
   "Path": "D:/LRH/Application/DTS Cloud/SDK",
   "UseHttps": false
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory , RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory , type RouteRecordRaw } from 'vue-router'
 import { HomeItem } from './modules/home'
 
 const routes: Array<RouteRecordRaw> = [
