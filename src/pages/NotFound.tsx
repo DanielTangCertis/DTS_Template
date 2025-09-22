c324577a24fa9340e97b41737275675bb98c1466
Daniel Tang <danieljq_tang@certisgroup.com>
2025-09-22 16:19:49 +0800

    not found page fix

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Box, styled } from '@mui/material';
-import { LayoutBox } from '../components/Layout';
+import React from "react";
+import { Box, styled } from "@mui/material";
+import { LayoutBox } from "../components/Layout";
 
 const NotFoundPageContainer = styled(Box)({
-  width: '100%',
-  height: '100%',
-  position: 'relative',
-  overflow: 'hidden',
-  fontSize: '3rem',
+  width: "100%",
+  height: "100%",
+  position: "relative",
+  overflow: "hidden",
+  fontSize: "3rem",
 });
 
 const NotFound: React.FC = () => {
   return (
     <NotFoundPageContainer>
-      <LayoutBox>404 Not Found</LayoutBox>
+      <Box>404 Not Found</Box>
     </NotFoundPageContainer>
   );
 };
 
-export default NotFound
+export default NotFound;
