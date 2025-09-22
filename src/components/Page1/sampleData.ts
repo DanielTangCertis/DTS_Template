c997e2ecc7ef355d577db2376352ae41f82295bd
Daniel Tang <danieljq_tang@certisgroup.com>
2025-09-22 16:53:05 +0800

    added nivo sample chart

--- src/components/Page1/sampleData.ts
+++ src/components/Page1/sampleData.ts
@@ -121,3 +121,273 @@ export const barSampleData2 = () => {
     { id: "cat5", value: 87 },
   ];
 };
+
+export const lineSampleData2 = () => {
+  return [
+    {
+      id: "japan",
+      data: [
+        {
+          x: "plane",
+          y: 234,
+        },
+        {
+          x: "helicopter",
+          y: 20,
+        },
+        {
+          x: "boat",
+          y: 294,
+        },
+        {
+          x: "train",
+          y: 260,
+        },
+        {
+          x: "subway",
+          y: 237,
+        },
+        {
+          x: "bus",
+          y: 43,
+        },
+        {
+          x: "car",
+          y: 220,
+        },
+        {
+          x: "moto",
+          y: 266,
+        },
+        {
+          x: "bicycle",
+          y: 45,
+        },
+        {
+          x: "horse",
+          y: 58,
+        },
+        {
+          x: "skateboard",
+          y: 188,
+        },
+        {
+          x: "others",
+          y: 26,
+        },
+      ],
+    },
+    {
+      id: "france",
+      data: [
+        {
+          x: "plane",
+          y: 116,
+        },
+        {
+          x: "helicopter",
+          y: 7,
+        },
+        {
+          x: "boat",
+          y: 230,
+        },
+        {
+          x: "train",
+          y: 96,
+        },
+        {
+          x: "subway",
+          y: 0,
+        },
+        {
+          x: "bus",
+          y: 77,
+        },
+        {
+          x: "car",
+          y: 116,
+        },
+        {
+          x: "moto",
+          y: 152,
+        },
+        {
+          x: "bicycle",
+          y: 59,
+        },
+        {
+          x: "horse",
+          y: 260,
+        },
+        {
+          x: "skateboard",
+          y: 280,
+        },
+        {
+          x: "others",
+          y: 288,
+        },
+      ],
+    },
+    {
+      id: "us",
+      data: [
+        {
+          x: "plane",
+          y: 194,
+        },
+        {
+          x: "helicopter",
+          y: 173,
+        },
+        {
+          x: "boat",
+          y: 114,
+        },
+        {
+          x: "train",
+          y: 213,
+        },
+        {
+          x: "subway",
+          y: 275,
+        },
+        {
+          x: "bus",
+          y: 150,
+        },
+        {
+          x: "car",
+          y: 175,
+        },
+        {
+          x: "moto",
+          y: 189,
+        },
+        {
+          x: "bicycle",
+          y: 140,
+        },
+        {
+          x: "horse",
+          y: 140,
+        },
+        {
+          x: "skateboard",
+          y: 283,
+        },
+        {
+          x: "others",
+          y: 183,
+        },
+      ],
+    },
+    {
+      id: "germany",
+      data: [
+        {
+          x: "plane",
+          y: 277,
+        },
+        {
+          x: "helicopter",
+          y: 54,
+        },
+        {
+          x: "boat",
+          y: 240,
+        },
+        {
+          x: "train",
+          y: 290,
+        },
+        {
+          x: "subway",
+          y: 79,
+        },
+        {
+          x: "bus",
+          y: 191,
+        },
+        {
+          x: "car",
+          y: 272,
+        },
+        {
+          x: "moto",
+          y: 288,
+        },
+        {
+          x: "bicycle",
+          y: 76,
+        },
+        {
+          x: "horse",
+          y: 139,
+        },
+        {
+          x: "skateboard",
+          y: 187,
+        },
+        {
+          x: "others",
+          y: 72,
+        },
+      ],
+    },
+    {
+      id: "norway",
+      data: [
+        {
+          x: "plane",
+          y: 146,
+        },
+        {
+          x: "helicopter",
+          y: 92,
+        },
+        {
+          x: "boat",
+          y: 206,
+        },
+        {
+          x: "train",
+          y: 165,
+        },
+        {
+          x: "subway",
+          y: 219,
+        },
+        {
+          x: "bus",
+          y: 208,
+        },
+        {
+          x: "car",
+          y: 212,
+        },
+        {
+          x: "moto",
+          y: 84,
+        },
+        {
+          x: "bicycle",
+          y: 98,
+        },
+        {
+          x: "horse",
+          y: 115,
+        },
+        {
+          x: "skateboard",
+          y: 249,
+        },
+        {
+          x: "others",
+          y: 65,
+        },
+      ],
+    },
+  ];
+};
--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import { Box, styled } from '@mui/material';
-import { LayoutBox } from '../components/Layout';
-
+import React from "react";
+import { Box, styled } from "@mui/material";
+import { LayoutBox } from "../components/Layout";
+import { ResponsiveLine } from "@nivo/line";
+import { lineSampleData2 } from "@/components/Page1/sampleData";
 const Page2Container = styled(Box)({
-  width: '100%',
-  height: '100%',
-  position: 'relative',
-  overflow: 'hidden',
+  width: "100%",
+  height: "100%",
+  position: "relative",
+  overflow: "hidden",
 });
 
 const Page2: React.FC = () => {
   return (
     <Page2Container>
       <LayoutBox side="left" delay={0.3}>
-        <Box>Content</Box>
+        <ResponsiveLine /* or Line for fixed dimensions */
+          data={lineSampleData2()}
+          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+          yScale={{
+            type: "linear",
+            min: "auto",
+            max: "auto",
+            stacked: true,
+            reverse: false,
+          }}
+          curve="basis"
+          axisBottom={{ legend: "transportation", legendOffset: 36 }}
+          axisLeft={{ legend: "count", legendOffset: -40 }}
+          colors={{ scheme: "dark2" }}
+          pointSize={10}
+          pointColor={{ theme: "background" }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: "seriesColor" }}
+          pointLabelYOffset={-12}
+          enableTouchCrosshair={true}
+          useMesh={true}
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "column",
+              translateX: 100,
+              itemWidth: 80,
+              itemHeight: 22,
+              symbolShape: "circle",
+            },
+          ]}
+        />
       </LayoutBox>
       <LayoutBox side="right" delay={0.3}>
         <Box>Content</Box>
@@ -22,4 +54,4 @@ const Page2: React.FC = () => {
   );
 };
 
-export default Page2
+export default Page2;
